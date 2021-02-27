b2a8920f08ad8ea9b08b30567c2d9ef5ecaea22a
karelfila <karel.fila@educanet.cz>
2021-02-27 10:49:21 +0100

    hotovo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,25 +9,47 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = 0;
         int b = 0;
-        String ReadyZprava = "";
-        String name = "";
 
+        String name = "";
+        String ReadyZprava = "";
+        int Count = 0;
 
         try {
-                BufferedReader br = new BufferedReader(new FileReader("messages"));
-                ReadyZprava = br.readLine();
-            } catch (IOException e) {
+            BufferedReader br = new BufferedReader(new FileReader("messages"));
+            String nextLine = br.readLine();
+            while (nextLine != null) {
+                ReadyZprava += nextLine + "\n";
+                nextLine = br.readLine();
+                Count++;
+            }
+            br.close();
+        } catch (IOException e) {
             System.out.println("Bohužel se nepovedlo program načíst, zkuste to znovu později.");
         }
-
-
         String zprava;
         System.out.println("Zadej jméno:");
         name = sc.nextLine();
         System.out.println("----------------");
-        System.out.println(name + " máš tu zprávu: ");
+        if (Count == 0)
+            System.out.println(name + " nemáš tu žádné zprávy");
+        if (Count == 1) {
+            System.out.println(name + " máš tu " + Count + " zprávu:");
+        }
+        if (Count == 2) {
+            System.out.println(name + " máš tu " + Count + " zprávy:");
+        }
+        if (Count == 3) {
+            System.out.println(name + " máš tu " + Count + " zprávy:");
+        }
+        if (Count == 4) {
+            System.out.println(name + " máš tu " + Count + " zprávy:");
+        }
+        if (Count >= 5) {
+            System.out.println(name + " máš tu " + Count + " zpráv:");
+        }
         System.out.println(ReadyZprava);
         System.out.println("----------------");
+        System.out.println("Zadej zprávu:");
         while (a == 0) {
             zprava = sc.nextLine();
             if (zprava.equals("!konec")) {
